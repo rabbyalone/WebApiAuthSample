4ddf6f2d514f89c69f21c727b36f8353fa512374
asezer <basbas6189>
2015-06-12 18:53:54 -0400

    User Registration

--- WebApiAuthSample/Controllers/HomeController.cs
+++ WebApiAuthSample/Controllers/HomeController.cs
@@ -1,18 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http.Results;
 using System.Web.Mvc;
+using WebApiAuthSample.Models;
 
 namespace WebApiAuthSample.Controllers
 {
     public class HomeController : Controller
     {
+        private const string ApiUri = "https://localhost:44301/";
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<HttpStatusCodeResult> Register(string email, string password, string passwordConfirm)
+        {
+            if (String.IsNullOrEmpty(email) || 
+                String.IsNullOrEmpty(password) || 
+                !String.Equals(password,passwordConfirm))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, 
+                    "Registration form is invalid");
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var registerModel = new Dictionary<string, string>
+                {
+                    {"Email", email},
+                    {"Password", password},
+                    {"ConfirmPassword", password}
+                };
+
+                var response = await client.PostAsJsonAsync("api/Account/Register", registerModel);
+
+                if (response.IsSuccessStatusCode) 
+                    return new HttpStatusCodeResult(response.StatusCode);
+
+                var errors = await response.Content.ReadAsAsync<ResponseErrors>();
+                return new HttpStatusCodeResult(response.StatusCode, errors.ToString());
+            }
         }
     }
 }
---
+++ WebApiAuthSample/Models/ResponseErrors.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebApiAuthSample.Models
+{
+    public class ResponseErrors
+    {
+        public string Message { get; set; }
+        public Dictionary<string,string[]> ModelState { get; set; }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder(Message);
+            str.Append("<ul>");
+            foreach (var s in ModelState.SelectMany(err => err.Value))
+            {
+                str.AppendFormat("<li>{0}</li>", s);
+            }
+            str.Append("</ul>");
+
+            return str.ToString();
+        }
+    }
+}
