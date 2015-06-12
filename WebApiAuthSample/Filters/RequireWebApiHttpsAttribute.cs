205fd7b16f848d3978811593eafd5571b1cea58f
asezer <basbas6189>
2015-06-12 13:01:54 -0400

    Took out AllowInsecureHttp option

--- WebApiAuthSample/Filters/RequireWebApiHttpsAttribute.cs
+++ WebApiAuthSample/Filters/RequireWebApiHttpsAttribute.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
-using System.Web.Mvc;
 
 namespace WebApiAuthSample.Filters
 {
     public class RequireWebApiHttpsAttribute : AuthorizationFilterAttribute
     {
-        private const int Port = 43001;
-
         public override void OnAuthorization(HttpActionContext actionContext)
         {
             var request = actionContext.Request;
@@ -29,47 +25,4 @@ namespace WebApiAuthSample.Filters
             }
         }
     }
-
-
-    public class AbcAttribute : RequireHttpsAttribute
-    {
-        private readonly int _httpsPort;
-
-        public AbcAttribute()
-        {
-#if DEBUG
-            //set DEBUG port
-            _httpsPort = 44301;
-#else
-            //set Run port
-            _httpsPort = 443;
-#endif
-        }
-
-        protected override void HandleNonHttpsRequest(AuthorizationContext filterContext)
-        {
-            if (!string.Equals(filterContext.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
-            {
-                base.HandleNonHttpsRequest(filterContext);
-                //throw new InvalidOperationException("ToggleHttpHttpsAttribute can only be used on GET requests.");
-            }
-
-            var request = filterContext.HttpContext;
-
-            filterContext.Result = GetRedirectResult(request.Request);
-        }
-
-       private RedirectResult GetRedirectResult(HttpRequestBase request)
-       {
-           if (request.Url != null)
-           {
-               var uri = new UriBuilder(request.Url) {Scheme = Uri.UriSchemeHttps, Port = _httpsPort};
-
-               return new RedirectResult(uri.ToString(), true);
-           }
-
-           throw new InvalidOperationException("ToggleHttpHttpsAttribute can only be used on GET requests.");
-       }
-    }
-
 }
